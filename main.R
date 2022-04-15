cd52028acf8227a7bf67463f6225b613853e3a53
jlgingrich <66572834+jlgingrich@users.noreply.github.com>
2022-04-14 23:47:50 -0400

    Clearing things

--- main.R
+++ main.R
@@ -1,11 +1,3 @@
 # INSTALLATION
-# install.packages('spotifyr', dependencies = TRUE)
+install.packages('spotifyr', dependencies = TRUE)
 
-uris = c("2ryI0oeYtWavBUlJDP2GVV")
-
-# SCRIPT
-get_my_recently_played(authorization=oauth)
-
-df = data.frame(get_playlist_audio_features("jsanchioli", uris, authorization=access_token))
-new_df = select(df, c("track.name","danceability","energy","speechiness","liveness","valence"))
-write.csv(new_df, "creepy_crawley_breakdown.csv")
