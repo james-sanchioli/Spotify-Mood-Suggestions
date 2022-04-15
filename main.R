c8eac3fd45b21fdd1565ab10d777e487462e2fb9
jlgingrich <66572834+jlgingrich@users.noreply.github.com>
2022-04-14 22:04:40 -0400

    Update

--- main.R
+++ main.R
@@ -17,5 +17,15 @@ scopes = c(
 
 oauth = get_spotify_authorization_code(scope = scopes)
 
+# uris = c("45i51TCBS6sxl33wtMtZm4")
+# uris = c("2bJAHjVNPyb0Dsd6iEhBxP")
+uris = c("2ryI0oeYtWavBUlJDP2GVV")
+
 # SCRIPT
 get_my_recently_played(authorization=oauth)
+
+print(get_playlist("jsanchioli", uris, authorization=access_token))
+
+df = data.frame(get_playlist_audio_features("jsanchioli", uris, authorization=access_token))
+new_df = select(df, c("track.name","danceability","energy","speechiness","liveness","valence"))
+write.csv(new_df, "creepy_crawley_breakdown.csv")
