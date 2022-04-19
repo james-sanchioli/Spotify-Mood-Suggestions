3de5f71d856b757ee187155c6cd4fc04e5e80302
jlgingrich <66572834+jlgingrich@users.noreply.github.com>
2022-04-19 02:30:42 -0400

    Error handling and reorganization

--- generate_playlist_report.R
+++ generate_playlist_report.R
@@ -1,20 +1,45 @@
-suppressMessages(source("setup.R", echo = FALSE))
+suppressPackageStartupMessages(source("setup.R"))
 
-args = commandArgs(trailingOnly=TRUE)
-if (length(args)==0) {
-  stop("Rscript generate_playlist_report.R --args SPOTIFY_URL_FOR_PLAYLIST")
+args = commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  stop("Run 'Rscript generate_playlist_report.R --args SPOTIFY_URL_FOR_PLAYLIST'", call. = FALSE)
 } else {
   url = args[2]
 }
 
-playlist_id = str_match(url,"/([A-z0-9]+)\\?")[2]
+playlist_id = str_match(url, "/([A-z0-9]+)\\?")[2]
 
-df = data.frame(get_playlist_audio_features(playlist_uris=playlist_id))
+message("\nBegin getting audio features")
+df = tryCatch({
+  data.frame(get_playlist_audio_features(playlist_uris = playlist_id))
+}, error = function(e) {
+    stop("Run 'Rscript generate_playlist_report.R --args SPOTIFY_URL_FOR_PLAYLIST'", call. = FALSE)
+  }
+)
 
-name = df[1,"playlist_name"]
-filename = paste0("Reports/",str_replace_all(tolower(name)," ", "_"), ".csv")
+name = df[1, "playlist_name"]
+message(paste0("Got audio features for the playlist '", name, "'"))
 
-features_df = select(df, c("track.name","track.id","danceability","energy","speechiness","liveness","valence"))
-dir.create("Reports",showWarnings = FALSE)
+features_df = select(
+  df,
+  c(
+    "track.name",
+    "track.id",
+    "danceability",
+    "energy",
+    "speechiness",
+    "liveness",
+    "valence"
+  )
+)
+
+dir.create("Reports", showWarnings = FALSE)
+filename = paste0("Reports/", str_replace_all(tolower(name), " ", "_"), ".csv")
 write.csv(features_df, filename)
-message(paste0("\nCreated report for the playlist ", name, " in '", filename, "'\n"))
+message(paste0(
+  "\nCreated report for the playlist '",
+  name,
+  "' in '",
+  filename,
+  "'\n"
+))
--- setup.R
+++ setup.R
@@ -4,6 +4,8 @@ library(knitr)
 library(dplyr)
 library(stringr)
 
+options("httr_oauth_cache" = TRUE)
+
 Sys.setenv(SPOTIFY_CLIENT_ID = '8be2402f0a3b40bd9f636bd95ef2711d')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = 'b8112b6d16da4cf0ae0d8f13a3ac0cbc')
 
